44a93c298ed8dabe50e5f651576a8dd22d76c312
Artem <48246993+quertc@users.noreply.github.com>
2023-12-05 19:23:03 +0300

    fix: failed to dial address

--- entrypoints/op-batcher.sh
+++ entrypoints/op-batcher.sh
@@ -2,8 +2,8 @@
 
 cd /app/data/optimism/op-batcher
 ./bin/op-batcher \
-  --l2-eth-rpc=http://localhost:8545 \
-  --rollup-rpc=http://localhost:8547\
+  --l2-eth-rpc=http://op-geth:8545 \
+  --rollup-rpc=http://op-node:8547 \
   --poll-interval=1s \
   --sub-safety-margin=6 \
   --num-confirmations=1 \
@@ -11,7 +11,7 @@ cd /app/data/optimism/op-batcher
   --resubmission-timeout=30s \
   --rpc.addr=0.0.0.0 \
   --rpc.port=8548 \
-  --rpc.enable-admin\
+  --rpc.enable-admin \
   --max-channel-duration=1 \
   --l1-eth-rpc=$L1_RPC_URL \
   --private-key=$GS_BATCHER_PRIVATE_KEY
