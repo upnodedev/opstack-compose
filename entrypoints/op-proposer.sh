e7c48caf02c4c17338e111bbdb6ec0c540edae3e
Artem <48246993+quertc@users.noreply.github.com>
2023-12-13 19:47:18 +0300

    feat: integrate celestia support and change flags (need fixes)

--- entrypoints/op-proposer.sh
+++ entrypoints/op-proposer.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-$BIN_DIR/op-proposer \
-  --l2oo-address=$(cat $DEPLOYMENT_DIR/L2OutputOracleProxy.json | jq -r .address) \
+# Check if OP_PROPOSER_L2OO_ADDRESS environment variable is set
+if [ -z "$OP_PROPOSER_L2OO_ADDRESS" ]; then
+  # If not set, use the address from the $DEPLOYMENT_DIR
+  OP_PROPOSER_L2OO_ADDRESS=$(cat $DEPLOYMENT_DIR/L2OutputOracleProxy.json | jq -r .address)
+fi
+
+$BIN_DIR/op-proposer
