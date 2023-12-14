41f47bfd58f9c4b562df4effd0aba119ef64e3ca
Artem <48246993+quertc@users.noreply.github.com>
2023-12-14 18:03:27 +0300

    feat: separate celestia-da.sh script and fix missing password file

---
+++ entrypoints/celestia-da.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ACCNAME=acc
+
+# Variables for keyring and node store paths
+KEYRING_PATH="/home/celestia/$DA_FOLDER_NAME/keys/keyring-test/$ACCNAME.info"
+NODE_STORE_PATH="/home/celestia/$DA_FOLDER_NAME/config.toml"
+
+# Check if keyring exists and recover key if it doesn't
+if [ ! -f "$KEYRING_PATH" ]; then
+  echo "Keyring not found or empty. Recovering key..."
+
+  # Check if the mnemonic is provided
+  if [ -z "$DA_KEYRING_MNEMONIC" ]; then
+    echo "Error: Mnemonic not provided in DA_KEYRING_MNEMONIC"
+    exit 1
+  fi
+
+  # Use the mnemonic to recover the key
+  echo $DA_KEYRING_MNEMONIC | cel-key add $ACCNAME --recover --keyring-backend test --node.type light --keyring-dir /home/celestia/$DA_FOLDER_NAME/keys --p2p.network $DA_P2P_NETWORK
+fi
+
+# Check if the node store is initialized
+if [ ! -f $NODE_STORE_PATH ]; then
+  echo "Initializing Celestia Node Store as $NODE_STORE_PATH is missing..."
+  celestia-da light init --p2p.network=$DA_P2P_NETWORK --node.store /home/celestia/$DA_FOLDER_NAME/
+fi
+
+
+# Start the Celestia node
+celestia-da light start \
+  --da.grpc.namespace=$DA_GRPC_NAMESPACE \
+  --da.grpc.listen=0.0.0.0:26650 \
+  --core.ip $DA_CORE_IP \
+  --p2p.network=$DA_P2P_NETWORK \
+  --keyring.accname=$ACCNAME \
+  --gateway
--- entrypoints/op-geth.sh
+++ entrypoints/op-geth.sh
@@ -3,6 +3,15 @@
 # Initialize op-geth if datadir is empty
 if [ -d "$DATADIR_DIR" ] && [ -z "$(ls -A $DATADIR_DIR)" ]; then
   echo "Initializing op-geth as $DATADIR_DIR is empty..."
+
+  # Creating password file
+  echo "$PASSWORD" > "$DATADIR_DIR/password"
+
+  # Creating block signer key file and importing it
+  echo "$SEQUENCER_PRIVATE_KEY" > "$DATADIR_DIR/block-signer-key"
+  $BIN_DIR/geth account import --datadir="$DATADIR_DIR" --password="$DATADIR_DIR/password" "$DATADIR_DIR/block-signer-key"
+
+  # Initialize with genesis block
   $BIN_DIR/geth init --datadir=$DATADIR_DIR $CONFIG_PATH/genesis.json
 fi
 
