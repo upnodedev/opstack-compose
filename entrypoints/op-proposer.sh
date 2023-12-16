dbbb82e6f152a4f2d2a272fc653e41596b7dae1c
Artem <48246993+quertc@users.noreply.github.com>
2023-12-16 18:59:34 +0300

    fix: missing OP_BATCHER_PRIVATE_KEY and OP_PROPOSER_PRIVATE_KEY in services

--- entrypoints/op-proposer.sh
+++ entrypoints/op-proposer.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Check if OP_PROPOSER_PRIVATE_KEY environment variable is set
+if [ -z "$OP_PROPOSER_PRIVATE_KEY" ]; then
+  echo "OP_PROPOSER_PRIVATE_KEY are missing, fetching from AWS Secrets Manager..."
+  secrets=$(aws secretsmanager get-secret-value --secret-id $AWS_SECRET_ARN | jq '.SecretString | fromjson')
+
+  OP_PROPOSER_PRIVATE_KEY="$(echo "${secrets}" | jq -r '.PROPOSER_PRIVATE_KEY')"
+
+  export OP_PROPOSER_PRIVATE_KEY
+fi
+
 # Check if OP_PROPOSER_L2OO_ADDRESS environment variable is set
 if [ -z "$OP_PROPOSER_L2OO_ADDRESS" ]; then
   # If not set, use the address from the $DEPLOYMENT_DIR
