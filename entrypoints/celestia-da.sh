65de14a0a9beaa9fd09fcfafc345247d440ba492
upnodedev <upresearchEditor@gmail.com>
2024-02-26 09:51:14 +0700

    feat: enable other celestia da node type

--- entrypoints/celestia-da.sh
+++ entrypoints/celestia-da.sh
@@ -17,11 +17,11 @@ fi
 # Check if the node store is initialized
 if [ ! -f "$CELESTIA_NODE_STORE_CONFIG_PATH" ]; then
   echo "Initializing Celestia Node Store as $CELESTIA_NODE_STORE_CONFIG_PATH is missing..."
-  celestia-da light init --p2p.network="$DA_P2P_NETWORK" --node.store "$CELESTIA_NODE_STORE"/
+  celestia-da $CELESTIA_NODE_TYPE init --p2p.network="$DA_P2P_NETWORK" --node.store "$CELESTIA_NODE_STORE"/
 fi
 
 # Start the Celestia node
-exec celestia-da light start \
+exec celestia-da $CELESTIA_NODE_TYPE start \
   --da.grpc.namespace="$DA_GRPC_NAMESPACE" \
   --da.grpc.listen=0.0.0.0:26650 \
   --core.ip "$DA_CORE_IP" \
