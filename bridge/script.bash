6b270d502d490671e01cf9b3804e6df1555bc711
france <frongrider2@gmail.com>
2024-10-11 23:20:03 +0700

    add sub module bridge ui and indexer

--- bridge/script.bash
+++
@@ -1,6 +0,0 @@
-# delete the existing folder if it exists
-rm -rf bridge-indexer
-rm -rf bridge-ui
-
-git clone https://github.com/upnodedev/opstack-bridge-indexer-v2.git bridge-indexer
-git clone https://github.com/upnodedev/opstack-bridge-ui-v2.git bridge-ui
